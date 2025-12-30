4d2f2e1f579665469d3c7245ddc9b648d71c34f0
Alexander Hurd <hurdad@gmail.com>
2025-12-29 21:04:28 -0500

    Rename OTEL endpoint env var

--- runtime/src/metrics/otel_metrics.cc
+++ runtime/src/metrics/otel_metrics.cc
@@ -1,5 +1,8 @@
 #include "metrics/otel_metrics.h"
 
+#include <chrono>
+
+#ifdef FLOWPIPE_ENABLE_OTEL
 // OpenTelemetry API
 #include <opentelemetry/metrics/meter.h>
 #include <opentelemetry/metrics/meter_provider.h>
@@ -12,10 +15,7 @@
 #include <opentelemetry/sdk/resource/resource.h>
 
 // OTLP exporter
-#include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter.h>
-
-#include <chrono>
-#include <memory>
+#include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_factory.h>
 
 namespace otel_metrics = opentelemetry::metrics;
 namespace otel_sdk_metrics = opentelemetry::sdk::metrics;
@@ -24,16 +24,27 @@ namespace otel_otlp = opentelemetry::exporter::otlp;
 
 namespace flowpipe::metrics {
 
+namespace {
+
 // ---- Global OTEL objects (kept alive for process lifetime) ----
 
-static opentelemetry::nostd::shared_ptr<otel_metrics::MeterProvider> provider;
-static opentelemetry::nostd::shared_ptr<otel_metrics::Meter> meter;
+opentelemetry::nostd::shared_ptr<otel_metrics::MeterProvider> provider;
+opentelemetry::nostd::shared_ptr<otel_metrics::Meter> meter;
 
 // Instruments
-static otel_metrics::Counter<uint64_t>* flow_started = nullptr;
-static otel_metrics::Counter<uint64_t>* flow_completed = nullptr;
-static otel_metrics::Counter<uint64_t>* stage_processed = nullptr;
-static otel_metrics::UpDownCounter<int64_t>* queue_depth = nullptr;
+opentelemetry::nostd::shared_ptr<otel_metrics::Counter<uint64_t>> flow_started;
+opentelemetry::nostd::shared_ptr<otel_metrics::Counter<uint64_t>> flow_completed;
+opentelemetry::nostd::shared_ptr<otel_metrics::Counter<uint64_t>> stage_processed;
+opentelemetry::nostd::shared_ptr<otel_metrics::UpDownCounter<int64_t>> queue_depth;
+
+otel_resource::Resource CreateResource(const std::string& service_name) {
+  return otel_resource::Resource::Create({
+      {"service.name", service_name},
+      {"service.namespace", "flow-pipe"},
+  });
+}
+
+}  // namespace
 
 // -------------------------------------------------------------
 
@@ -41,58 +52,40 @@ void Metrics::Init(const std::string& service_name, const std::string& endpoint)
   // ---- Exporter ----
   otel_otlp::OtlpGrpcMetricExporterOptions exporter_opts;
   exporter_opts.endpoint = endpoint;
+  exporter_opts.use_ssl_credentials = false;
 
-  auto exporter = std::make_unique<otel_otlp::OtlpGrpcMetricExporter>(exporter_opts);
+  auto exporter = otel_otlp::OtlpGrpcMetricExporterFactory::Create(exporter_opts);
 
   // ---- Reader ----
-  otel_sdk_metrics::PeriodicExportingMetricReaderOptions peroid_options;
-  peroid_options.export_interval_millis = std::chrono::milliseconds{5000};
+  otel_sdk_metrics::PeriodicExportingMetricReaderOptions period_options;
+  period_options.export_interval_millis = std::chrono::milliseconds{5000};
 
   auto reader = std::make_unique<otel_sdk_metrics::PeriodicExportingMetricReader>(
-      std::move(exporter), peroid_options);
+      std::move(exporter), period_options);
 
-  // ---- Resource ----
-  auto resource = otel_resource::Resource::Create({
-      {"service.name", service_name},
-      {"service.namespace", "flow-pipe"},
-  });
+  // ---- Meter provider ----
+  auto sdk_provider = std::make_shared<otel_sdk_metrics::MeterProvider>();
+  sdk_provider->SetResource(CreateResource(service_name));
+  sdk_provider->AddMetricReader(std::move(reader));
 
-  // // ---- SDK Provider ----
-  // auto sdk_provider =
-  //     std::make_shared<otel_sdk_metrics::MeterProvider>(resource);
-  //
-  // sdk_provider->AddMetricReader(std::move(reader));
-  //
-  // // IMPORTANT:
-  // // OpenTelemetry API expects nostd::shared_ptr, not std::shared_ptr
-  // provider = opentelemetry::nostd::shared_ptr<otel_metrics::MeterProvider>(
-  //     sdk_provider);
-  //
-  // otel_metrics::Provider::SetMeterProvider(provider);
+  provider = opentelemetry::nostd::shared_ptr<otel_metrics::MeterProvider>(sdk_provider);
+  otel_metrics::Provider::SetMeterProvider(provider);
 
   // ---- Meter ----
   meter = provider->GetMeter("flowpipe.runtime", "1.0.0");
 
   // ---- Instruments ----
-  // flow_started =
-  //     &meter->CreateUInt64Counter(
-  //         "flow_started_total",
-  //         "Number of flows started");
-  //
-  // flow_completed =
-  //     &meter->CreateUInt64Counter(
-  //         "flow_completed_total",
-  //         "Number of flows completed");
-  //
-  // stage_processed =
-  //     &meter->CreateUInt64Counter(
-  //         "stage_processed_total",
-  //         "Number of stage executions");
-  //
-  // queue_depth =
-  //     &meter->CreateUpDownCounter(
-  //         "queue_depth",
-  //         "Current depth of queues");
+  flow_started = meter->CreateUInt64Counter(
+      "flow_started_total", "Number of flows started");
+
+  flow_completed = meter->CreateUInt64Counter(
+      "flow_completed_total", "Number of flows completed");
+
+  stage_processed = meter->CreateUInt64Counter(
+      "stage_processed_total", "Number of stage executions");
+
+  queue_depth = meter->CreateInt64UpDownCounter(
+      "queue_depth", "Current depth of queues");
 }
 
 void Metrics::Shutdown() {
@@ -129,3 +122,23 @@ void Metrics::QueueDepth(const std::string& queue, int64_t depth) {
 }
 
 }  // namespace flowpipe::metrics
+
+#else  // FLOWPIPE_ENABLE_OTEL
+
+namespace flowpipe::metrics {
+
+void Metrics::Init(const std::string&, const std::string&) {}
+
+void Metrics::Shutdown() {}
+
+void Metrics::FlowStarted() {}
+
+void Metrics::FlowCompleted() {}
+
+void Metrics::StageProcessed(const std::string&) {}
+
+void Metrics::QueueDepth(const std::string&, int64_t) {}
+
+}  // namespace flowpipe::metrics
+
+#endif  // FLOWPIPE_ENABLE_OTEL
