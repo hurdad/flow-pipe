6b15ecda82485c426993517feb0adad6f83560c4
Alexander Hurd <hurdad@gmail.com>
2026-01-14 10:15:54 -0500

    Unblock queues on signal shutdown

--- runtime/include/flowpipe/signal_handler.h
+++ runtime/include/flowpipe/signal_handler.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <atomic>
+#include <functional>
 
 namespace flowpipe {
 
 class SignalHandler {
  public:
-  static void install(std::atomic<bool>& stop_flag);
+  static void install(std::atomic<bool>& stop_flag, std::function<void()> on_stop);
 };
 
 }  // namespace flowpipe
