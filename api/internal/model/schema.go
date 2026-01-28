cd461ebd54ed85e4876f6840c56213657b6001c5
Alexander Hurd <hurdad@gmail.com>
2026-01-27 23:49:36 -0500

    update

--- api/internal/model/schema.go
+++ api/internal/model/schema.go
@@ -7,6 +7,6 @@ import (
 type SchemaDefinition struct {
 	SchemaID  string                       `json:"schema_id"`
 	Version   uint32                       `json:"version"`
-	Format    flowpipev1.QueueSchemaFormat `json:"format"`
+	Format    flowpipev1.InMemorySchemaFormat `json:"format"`
 	RawSchema []byte                       `json:"raw_schema"`
 }
